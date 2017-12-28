08fd7f2f2b8f8748254459f664033bf14b0e1807
Julik Tarkhanov <me@julik.nl>
2017-12-28 16:06:45 +0100

    Add the MOOV spec sketch

---
+++ spec/parsers/moov_parser_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe FormatParser::MOOVParser do
+  it 'parses an MP4 file and provides the necessary metadata'
+  it 'parses an M4A file and provides the necessary metadata'
+  it 'parses a MOV file and provides the necessary metadata'
+end
