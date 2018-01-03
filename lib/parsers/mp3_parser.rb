ca3b53b99df203b5029c5d69a1ddcfc7201bb0a4
Noah Berman <noah@wetransfer.com>
2018-01-03 17:00:29 +0100

    Methods that don't do anything

--- lib/parsers/mp3_parser.rb
+++ lib/parsers/mp3_parser.rb
@@ -3,7 +3,24 @@ class FormatParser::MP3Parser
 
   def information_from_io(io)
     io.seek(0)
-    return
+    frame_header = safe_read(io, 13)
+    # If the frame_header includes ID3, the header is wrapped in an ID3v2 tag
+    parse_id3_v2_tag(io) if frame_header.include?("ID3")
+  end
+
+  def parse_id3_v2_tag(io)
+    io.seek(0)
+    # Parse the first few chunks of the ID3 tag. This gets tricky because
+    # the nature of the tag is such that it's sometimes difficult to tell
+    # exactly when there's no more tag left to read
+  end
+
+  def parse_id3_v1_tag(io)
+    io.seek(0)
+  end
+
+  def parse_mp3_without_id3_tag(io)
+    io.seek(0)
   end
 
   FormatParser.register_parser_constructor self
